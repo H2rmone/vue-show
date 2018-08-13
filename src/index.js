49c64043e3c97ffe9aa000bb780d8b378539de5b
H2rmone <3357458@qq.com>
2018-08-13 19:56:53 +0800

    chore: update version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import xShow from './x-show'
 import easings from './easings'
 
-const version = '1.0.4'
+const version = '1.0.8'
 
 const install = function (Vue) {
   Vue.component(xShow.name, xShow)
